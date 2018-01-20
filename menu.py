811c675dae96e8d6e840a453cd1a4813800676b7
huidanxiao <xhuidan2@illinois.edu>
2018-01-19 22:38:24 -0600

    menu class finish half

--- menu.py
+++ menu.py
@@ -1,3 +1,6 @@
+from database import Database
+from models.blog import Blog
+
 __author__="Huidan Xiao"
 
 class Menu(object):
@@ -5,7 +8,29 @@ class Menu(object):
        #Ask user for user name
        #check if they have already got an account
        #if not, prompt them to creat one
-       pass
+       self.user=input("enter your author name")
+       self.user_blog=None
+       if self._user_has_account():
+           print("welcome back {}".format(self.user))
+       else:
+           self._prompt_user_for_account()
+
+    def _user_has_account(self):
+        blog=Database.find_one("blogs",{"author":self.user})
+        if blog is not None:
+            self.user_blog=blog
+            return True
+        else:
+            return False
+
+    def _promt_user_for_account(self):
+        title=input("enter your title")
+        description=input("enter your description")
+        blog=Blog(author=self.user,
+                  title=title,
+                  description=description)
+        blog.save_to_mongo()
+        self.user_blog=blog
 
     def run_menu(self):
         #user read or write blogs?
@@ -17,4 +42,13 @@ class Menu(object):
             #check if user has a blog
             #if they do, prompt to have a post
             #if not, prompt them to have a new blog
+
+        read_or_write = input("do you want to read(R) or write(W) blogs?")
+        if read_or_write=="R":
+            pass
+        elif read_or_write=="W":
+            pass
+        else:
+            print("thank you for logging!")
+
         pass
